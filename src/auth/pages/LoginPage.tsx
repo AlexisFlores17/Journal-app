2c6be4afd66631433b0de316ed30747546ca064e
AlexisFlores17 <alex.p.flores17@gmail.com>
2025-02-16 00:41:33 -0600

    feat: :art: Login and register page
    Structure for login and register page

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,68 +1,53 @@
 import { Google } from "@mui/icons-material";
 import { Button, Grid2, Link, TextField, Typography } from "@mui/material";
-import {Link as RouterLink} from "react-router";
+import { Link as RouterLink } from "react-router";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const LoginPage = () => {
   return (
-    <Grid2
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
-    >
-      <Grid2
-        className="box-shadow"
-        size={{ xs: 12, sm: 8, md: 5 }}
-        sx={{ backgroundColor: "white", padding: 3, borderRadius: 2 }}
-      >
-        <Typography variant="h5" sx={{ mb: 1 }}>
-          Login
-        </Typography>
+    <AuthLayout title="Login">
+      <form action="">
+        <Grid2 container>
+          <Grid2 size={{ xs: 12 }} sx={{ mb: 2 }}>
+            <TextField
+              variant="filled"
+              label="Correo"
+              type="email"
+              placeholder="correo@mail.com"
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 size={{ xs: 12 }} sx={{ mb: 2 }}>
+            <TextField
+              variant="filled"
+              label="Password"
+              type="password"
+              placeholder="Contraseña"
+              autoComplete="off"
+              fullWidth
+            />
+          </Grid2>
 
-        <form action="">
-          <Grid2 container>
-            <Grid2 size={{ xs: 12 }} sx={{ mb: 2 }}>
-              <TextField
-                variant="filled"
-                label="Correo"
-                type="email"
-                placeholder="correo@mail.com"
-                fullWidth
-              />
+          <Grid2 container spacing={2} sx={{ mb: 2, mt: 1 }} size={12}>
+            <Grid2 size={{ xs: 12, md: 6 }}>
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
             </Grid2>
-            <Grid2 size={{ xs: 12 }} sx={{ mb: 2 }}>
-              <TextField
-                variant="filled"
-                label="Password"
-                type="password"
-                placeholder="Contraseña"
-                fullWidth
-              />
-            </Grid2>
-
-            <Grid2 container spacing={2} sx={{ mb: 2, mt:1 }} size={12}>
-              <Grid2 size={{ xs: 12, md: 6 }}>
-                <Button variant="contained" fullWidth>
-                  Login
-                </Button>
-              </Grid2>
-              <Grid2 size={{ xs: 12, md: 6 }}>
-                <Button variant="contained" fullWidth>
-                  <Google />
-                  <Typography sx={{ ml: 1 }}>Google</Typography>
-                </Button>
-              </Grid2>
-            </Grid2>
-            <Grid2 container direction="row" justifyContent="end" size={12}>
-              <Link color="inherit" to="/auth/register" component={RouterLink}>
-                Crear una cuenta
-              </Link>
+            <Grid2 size={{ xs: 12, md: 6 }}>
+              <Button variant="contained" fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
             </Grid2>
           </Grid2>
-        </form>
-      </Grid2>
-    </Grid2>
+          <Grid2 container direction="row" justifyContent="end" size={12}>
+            <Link color="inherit" to="/auth/register" component={RouterLink}>
+              Crear una cuenta
+            </Link>
+          </Grid2>
+        </Grid2>
+      </form>
+    </AuthLayout>
   );
 };
--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,7 +1,55 @@
-
+import { Grid2, TextField, Button, Typography, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <AuthLayout title="Register">
+      <form action="">
+        <Grid2 container>
+          <Grid2 size={{ xs: 12 }} sx={{ mb: 2 }}>
+            <TextField
+              variant="filled"
+              label="Nombre"
+              type="text"
+              placeholder="Usuario"
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 size={{ xs: 12 }} sx={{ mb: 2 }}>
+            <TextField
+              variant="filled"
+              label="Correo"
+              type="email"
+              placeholder="correo@mail.com"
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 size={{ xs: 12 }} sx={{ mb: 2 }}>
+            <TextField
+              variant="filled"
+              label="Password"
+              type="password"
+              placeholder="Contraseña"
+              autoComplete="off"
+              fullWidth
+            />
+          </Grid2>
+          <Grid2  spacing={2} sx={{ mb: 2, mt: 1 }} size={12}>
+            <Grid2 size={{ xs: 12 }}>
+              <Button variant="contained" fullWidth>
+                Crear cuenta
+              </Button>
+            </Grid2>
+          </Grid2>
+          <Grid2 container size={{ xs: 12 }} direction="row" justifyContent="end">
+            <Typography sx={{mr:1}} >¿Ya tienes cuenta?</Typography>
+            <Link component={RouterLink} to="/auth/login">
+              ingresar
+            </Link>
+          </Grid2>
+        </Grid2>
+      </form>
+    </AuthLayout>
+  );
+};
