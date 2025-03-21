a404ac6ed88652bfae3f1615ac579d103949252f
AlexisFlores17 <35354528+AlexisFlores17@users.noreply.github.com>
2025-03-21 01:17:52 -0600

    feat: :lipstick: AddButton note
    Style for add note button

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,8 +1,9 @@
 
 
-import { Toolbar } from "@mui/material"
+import { IconButton, Toolbar } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
   return (
@@ -10,6 +11,20 @@ export const JournalPage = () => {
       <Toolbar />
         {/* <NothingSelectedView /> */}
         <NoteView />
+
+        <IconButton
+          size="large"
+          sx={{
+            color:"white",
+            backgroundColor:"error.main",
+            ":hover":{ backgroundColor:"error.main", opacity:0.9},
+            position:"fixed",
+            right:50,
+            bottom:50
+          }}
+        >
+          <AddOutlined sx={{fontSize:30}}/>
+        </IconButton>
     </JournalLayout>
   )
 }
