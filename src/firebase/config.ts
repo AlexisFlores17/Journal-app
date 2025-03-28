7804637eec897001fceb9bef190b7ebc1a4b75cd
AlexisFlores17 <35354528+AlexisFlores17@users.noreply.github.com>
2025-03-28 00:39:30 -0600

    feat: :sparkles: db
    db setup

---
+++ src/firebase/config.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD37ASOnOgSlMPp5uSnwiJwJIgYnmhpTNA",
+  authDomain: "react-curso-fe5bb.firebaseapp.com",
+  projectId: "react-curso-fe5bb",
+  storageBucket: "react-curso-fe5bb.firebasestorage.app",
+  messagingSenderId: "1057579018957",
+  appId: "1:1057579018957:web:ec4174567964020d849784"
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebaseDB = getFirestore(FirebaseApp);
