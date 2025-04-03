01c6a6bdd5c9b3badbe1fe1abd3456d1b77e65a3
AlexisFlores17 <35354528+AlexisFlores17@users.noreply.github.com>
2025-04-03 17:14:17 -0600

    feat: :sparkles: Form validations
    validations for  the form elements

--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,11 +1,43 @@
 import { Grid2, TextField, Button, Typography, Link } from "@mui/material";
 import { Link as RouterLink } from "react-router";
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks";
+import { useState } from "react";
+
+const formData = {
+  email: "alex@google.com",
+  password: "123456",
+  displayName: "Nombre de usuario",
+};
+interface FormState {
+  email: string;
+  password: string;
+  displayName: string;
+}
+
+const formValidations: Record<string, [(value: string) => boolean, string?]> = {
+  email: [(value: string) => value.includes("@"), "El correo debe de tener un @"],
+  password: [(value: string) => value.length >= 6, "El password debe de tener más de 6 letras"],
+  displayName: [(value: string) => value.length >= 1, "El nombre es obligatorio"],
+};
 
 export const RegisterPage = () => {
+
+  const [formSubmited, setFormSubmited] = useState(false)
+
+  const { displayName, email, password, onInputChange, formState , displayNameValid, emailValid, passwordValid } =
+  useForm(formData, formValidations);
+
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+
+    setFormSubmited(true);
+    console.log(formState as FormState);
+  };
   return (
     <AuthLayout title="Register">
-      <form action="">
+      <form onSubmit= {onSubmit}>
         <Grid2 container>
           <Grid2 size={{ xs: 12 }} sx={{ mb: 2 }}>
             <TextField
@@ -14,6 +46,12 @@ export const RegisterPage = () => {
               type="text"
               placeholder="Usuario"
               fullWidth
+              name="displayName"
+              value={displayName}
+              onChange = {onInputChange}
+              error={!!displayNameValid && formSubmited}
+              helperText={displayNameValid}
+
             />
           </Grid2>
           <Grid2 size={{ xs: 12 }} sx={{ mb: 2 }}>
@@ -23,6 +61,11 @@ export const RegisterPage = () => {
               type="email"
               placeholder="correo@mail.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange = {onInputChange }
+              error={!!emailValid  && formSubmited}
+              helperText={emailValid}
             />
           </Grid2>
           <Grid2 size={{ xs: 12 }} sx={{ mb: 2 }}>
@@ -33,17 +76,27 @@ export const RegisterPage = () => {
               placeholder="Contraseña"
               autoComplete="off"
               fullWidth
+              name="password"
+              value={password}
+              onChange = {onInputChange}
+              error={!!passwordValid  && formSubmited}
+              helperText={passwordValid}
             />
           </Grid2>
-          <Grid2  spacing={2} sx={{ mb: 2, mt: 1 }} size={12}>
+          <Grid2 spacing={2} sx={{ mb: 2, mt: 1 }} size={12}>
             <Grid2 size={{ xs: 12 }}>
-              <Button variant="contained" fullWidth>
+              <Button  variant="contained" fullWidth type="submit">
                 Crear cuenta
               </Button>
             </Grid2>
           </Grid2>
-          <Grid2 container size={{ xs: 12 }} direction="row" justifyContent="end">
-            <Typography sx={{mr:1}} >¿Ya tienes cuenta?</Typography>
+          <Grid2
+            container
+            size={{ xs: 12 }}
+            direction="row"
+            justifyContent="end"
+          >
+            <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
             <Link component={RouterLink} to="/auth/login">
               ingresar
             </Link>
--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,13 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 interface FormState {
     email: string;
     password: string;
+    displayName?: string;
 }
 
-export const useForm = ( initialForm:FormState ) => {
+interface FormValidations {
+    [key: string]: [(value: string) => boolean, string?];
+}
+
+export const useForm = ( initialForm: FormState, formValidations: FormValidations = {} ) => {
   
     const [ formState, setFormState ] = useState( initialForm );
+    const [formValidation, setFormValidation] = useState<Record<string, string | null>>({});
+
+    useEffect(() => {
+      
+        createValidators();
+    
+      return () => {
+        
+      }
+    }, [formState])
+
+
+    const isFormValid = useMemo(() => {
+
+
+        for (const formValue of Object.keys( formValidation)) {
+            if( formValidation[formValue] !==null ) return false;
+        }
+
+        return true;
+
+    },[formValidation])
+    
 
     const onInputChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = target;
@@ -21,10 +49,27 @@ export const useForm = ( initialForm:FormState ) => {
         setFormState( initialForm );
     }
 
+    const createValidators = () => {
+        const formCheckedValues: Record<string, string | null> = {};
+
+        for (const formField of Object.keys(formValidations)) {
+            const [ fn , errorMessage ="Este campo es requerido" ] = formValidations[ formField];
+            
+            formCheckedValues[`${formField}Valid`] =  fn(formState[formField as keyof FormState] || '') ? null: errorMessage;
+        }
+
+        setFormValidation( formCheckedValues );
+    }
+
     return {
         ...formState,
         formState,
         onInputChange,
         onResetForm,
+        ...formValidation,
+        isFormValid,
+        displayNameValid: formValidations.displayName?.[0](formState.displayName || '') ? null : formValidations.displayName?.[1],
+        emailValid: formValidations.email?.[0](formState.email) ? null : formValidations.email?.[1],
+        passwordValid: formValidations.password?.[0](formState.password) ? null : formValidations.password?.[1],
     }
 }
